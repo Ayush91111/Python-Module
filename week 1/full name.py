a96f429c102d2433c52b53b611cecb574ade38d5
Ayush91111 <Ayush.Karki@westcliff.edu>
2026-05-12 15:23:05 +0545

    Add files via upload

---
+++ week 1/full name.py
@@ -0,0 +1,5 @@
+full_name = input("Enter your full name: ")
+parts = full_name.split()
+first_initial = parts[0][0]
+last_initial = parts[-1][0]
+print(f"Your initials are: {first_initial.upper()}.{last_initial.upper()}")
